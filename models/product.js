3834400b2a3ad5078dfe057a8443b328a0dfebf8
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-07-26 15:24:32 +0530

    mongo dB start and util is not uploaded for security concern of mine

--- models/product.js
+++ models/product.js
@@ -1,27 +1,33 @@
-const Sequelize = require('sequelize');
+const { getDb } = require("../util/database");
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
-});
+
+  async save() {
+    try {
+      const db = getDb();
+      const result = await db.collection("products").insertOne(this);
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async fetchAll() {
+    try {
+      const db = getDb();
+      const products = await db.collection("products").find().toArray();
+      console.log(products);
+      return products;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
 
 module.exports = Product;
