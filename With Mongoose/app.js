756c8a03dce508912903f398777bb77e35cf6993
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-07-28 06:33:16 +0530

    mongoose: ODM
    ODM: object-data-modeling, shop (edit, delete), etc. are working

---
+++ With Mongoose/app.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const errorController = require("./controllers/error");
+// const User = require('./models/user');
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // ! for new topology engine (nahi toh deprecated batata)
+  })
+  .then((result) => {
+    app.listen(3000);
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
